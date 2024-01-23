95670bc31465134cfd80311faec6b6f901ebfc97
FancyGoZheng <651792995@qq.com>
2024-01-23 23:48:30 +0800

    my commit

---
+++ PPT/test_report/data/slide23.js
@@ -0,0 +1,1 @@
+(function(){var loadHandler=window['sl_{DEB0DB0D-A9FE-4BDF-9821-A25F6516CBFC}'];loadHandler&&loadHandler(22, '<div id="spr0_100f9965"><div id="spr1_100f9965" class="kern slide"><img id="img0_100f9965" src="data/img1.png" width="5000px" height="2812px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_100f9965" class="kern slide"></div></div>', '{"s":[]}');})();
